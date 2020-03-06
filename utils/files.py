1a5500eab18c278136bcbaed8aa7a7d311195e4a
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2020-03-06 02:05:32 -0300

    Função para gerar caminho absoluto do arquivo

--- utils/files.py
+++ utils/files.py
@@ -3,3 +3,7 @@ import os
 
 def digest_exists_in_file(file_content, hexdigest):
     return hexdigest in file_content
+
+
+def get_absolute_file_path(file_name, path):
+    return os.join(path, file_name)
