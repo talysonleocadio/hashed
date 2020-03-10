1342c6bb806ecbbdb9c4b25c03c057b43373f42e
Talyson Leocadio <talyson.leocadio@climas.waycarbon.com>
2020-03-09 21:13:59 -0300

    Criando funções para falharem nos testes.

--- utils/files.py
+++ utils/files.py
@@ -7,3 +7,12 @@ def digest_exists_in_file(file_content, hexdigest):
 
 def get_absolute_file_path(file_name, path):
     return os.join(path, file_name)
+
+
+def file_exists(file_path):
+    return False
+
+
+def append_content_to_file(file_path, content):
+    return False
+
