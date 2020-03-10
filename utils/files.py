7110b8a00fce9f95e2ce8ac8406c29612f4ab0de
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2020-03-10 01:08:40 -0300

    Corrigindo ordem de declaração.

--- utils/files.py
+++ utils/files.py
@@ -5,8 +5,8 @@ def digest_exists_in_file(file_content, hexdigest):
     return hexdigest in file_content
 
 
-def get_absolute_file_path(file_name, path):
-    return os.join(path, file_name)
+def get_absolute_file_path(path, file_name):
+    return os.path.join(path, file_name)
 
 
 def file_exists(file_path):
