e38c2b9659b0af1809fd22c17ad8d2bfd1f3f019
Talyson Leocadio <talyson.leocadio@climas.waycarbon.com>
2020-03-09 21:12:30 -0300

    Mudando o nome e declaração do tempfile.

--- tests/utils/test_files.py
+++ tests/utils/test_files.py
@@ -10,8 +10,8 @@ class TestFileModuleFunctions(TestCase):
 
     def setUp(self):
         self.temp_path = gettempdir()
-        self.tempfile = NamedTemporaryFile(suffix='.txt')
-        self.tempfile_name = self.tempfile.name.split('/')[-1]
+        self.temp_file = NamedTemporaryFile(mode='w+', suffix='.txt')
+        self.tempfile_name = self.temp_file.name.split('/')[-1]
 
     def test_file_content_comparation_where_content_exists(self):
         file_content = ["digest_one", "digest_two", "digest_three"]
