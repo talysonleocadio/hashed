e4a7d2450603426dcfb6de9af0bb16c607416513
Talyson Leocadio <talyson.leocadio@climas.waycarbon.com>
2020-03-11 12:41:36 -0300

    Mudando estratégia de importação.

--- tests/utils/test_hashes.py
+++ tests/utils/test_hashes.py
@@ -1,22 +1,23 @@
-from unittest import TestCase, main
-from utils.hashes import _from_string_to_byte, sha1_hex_digest
+import unittest
+
+import utils.hashes as hash_utils
 
 
-class TestHashingModuleFunctions(TestCase):
+class TestHashingModuleFunctions(unittest.TestCase):
 
     def setUp(self):
         self.string = "bytes!"
 
     def test_string_convertion_to_byte_object(self):
         expected_byte_obj = b"bytes!"
-        byte_object = _from_string_to_byte(self.string)
+        byte_object = hash_utils._from_string_to_byte(self.string)
 
         self.assertEqual(expected_byte_obj,
                          byte_object,
                          "Byte objects dont match!")
 
     def test_different_byte_objects_dont_match(self):
-        byte_object = _from_string_to_byte(self.string)
+        byte_object = hash_utils._from_string_to_byte(self.string)
         different_byte_obj = b"is not bytes!"
 
         self.assertNotEqual(different_byte_obj,
@@ -24,7 +25,7 @@ class TestHashingModuleFunctions(TestCase):
                             "Byte objects are equal!")
 
     def test_sha1_hex_digest_generation(self):
-        digest = sha1_hex_digest(self.string)
+        digest = hash_utils.sha1_hex_digest(self.string)
         expected_digest = "9d1cf02da81f072e2b08b9c78ba4e07af997b205"
 
         self.assertEqual(digest,
@@ -32,14 +33,14 @@ class TestHashingModuleFunctions(TestCase):
                          "Digests dont match!")
 
     def test_different_digests_dont_match(self):
-        digest = sha1_hex_digest(self.string)
+        digest = hash_utils.sha1_hex_digest(self.string)
         different_string = "is not bytes!"
-        different_digest = sha1_hex_digest(different_string)
+        different_digest = hash_utils.sha1_hex_digest(different_string)
 
         self.assertNotEqual(digest,
                             different_digest,
                             "Digests are equal!")
 
     if __name__ == '__main__':
-        main()
+        unittest.main()
 
