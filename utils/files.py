4cd6774fef5a966f39294f64b8fefd4adf917022
Talyson Leocadio <talyson.leocadio@climas.waycarbon.com>
2020-04-07 09:04:26 -0300

    Função para retornar o conteúdo de algum arquivo.

--- utils/files.py
+++ utils/files.py
@@ -16,3 +16,8 @@ def file_exists(abs_file_path):
 def append_content_to_file(file_path, content):
     with open(file_path, 'a+') as file:
         file.write(content)
+
+
+def get_file_content(file_path):
+    with open(file_path, 'r') as file:
+        return file.readlines
